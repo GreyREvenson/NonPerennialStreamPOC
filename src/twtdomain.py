b4c0ef1d221093bf2eef4c05715eb8913df610d5
GreyREvenson <evenson.grey@epa.gov>
2026-01-21 13:27:44 -0500

    fix bug in domain def

--- src/twtdomain.py
+++ src/twtdomain.py
@@ -7,53 +7,60 @@ def set_domain(namelist:twtnamelist.Namelist):
     _set_paths(namelist)
 
 def _set_domain(namelist:twtnamelist.Namelist):
-    """Set domain - TODO: change so that domain can be a non-huc area and subdomain units can be non-huc area"""
+    """Set domain"""
     if namelist.options.verbose: print('calling _set_domain_boundary')
     if not os.path.isfile(namelist.fnames.domain) or namelist.options.overwrite:
-        try:
-            huc_lvl = int(len(namelist.options.domain_hucid))
-            if isinstance(namelist.options.huc_break_lvl,int) and namelist.options.huc_break_lvl in (2,4,6,8,10,12): 
-                huc_lvl = namelist.options.huc_break_lvl
-            colnam = 'huc' + str(len(namelist.options.domain_hucid))
-            hucs   = pygeohydro.WBD(colnam) 
-            ids    = hucs[colnam].unique().tolist()
-            ids    = [id for id in ids if id.startswith(namelist.options.domain_hucid)]
-            domain = hucs.byids(colnam, ids)
-            domain = domain.drop(columns=[col for col in domain.columns if col not in [colnam,'geometry']]) 
-            domain = domain.rename(columns = {colnam : 'domain_id'})
-            domain.to_file(namelist.fnames.domain, driver='GPKG')
-            return
-        except Exception as e_id:
-            pass
-        try:
-            geom = shapely.geometry.box(namelist.options.domain_bbox[0],
-                                        namelist.options.domain_bbox[1],
-                                        namelist.options.domain_bbox[2],
-                                        namelist.options.domain_bbox[3])
-            huc_lvl = namelist.options.huc_break_lvl if namelist.options.huc_break_lvl in (2,4,6,8,10,12) else 12
-            colnam = f'huc{huc_lvl}'
-            hucs   = pygeohydro.WBD(colnam)
-            domain = hucs.bygeom(geom)
-            domain = domain.drop(columns=[col for col in domain.columns if col not in [colnam,'geometry']]) 
-            domain = domain.rename(columns={colnam : 'domain_id'})
-            domain.to_file(namelist.fnames.domain, driver='GPKG')
-            return
-        except Exception as e_bbox:
-            pass
-        try:
-            geom = shapely.geometry.Point(namelist.options.domain_latlon[0],
-                                          namelist.options.domain_latlon[1]).buffer(0.01)
-            huc_lvl = namelist.options.huc_break_lvl if namelist.options.huc_break_lvl in (2,4,6,8,10,12) else 12
-            colnam = f'huc{huc_lvl}'
-            hucs   = pygeohydro.WBD(colnam)
-            domain = hucs.bygeom(geom)
-            domain = domain.drop(columns=[col for col in domain.columns if col not in [colnam,'geometry']]) 
-            domain = domain.rename(columns={colnam : 'domain_id'})
-            domain.to_file(namelist.fnames.domain, driver='GPKG')
-            return
-        except Exception as e_latlon:
-            pass
-        sys.exit(f'ERROR _set_domain_boundary could not set domain from domain_hucid error:{e_id}, domain_bbox error:{e_bbox}, domain_latlon error:{e_latlon}')
+        if len(str(namelist.options.domain_hucid)) > 0:
+            if namelist.options.verbose: print(f'  setting domain from domain_hucid {namelist.options.domain_hucid}')
+            try:
+                if isinstance(namelist.options.huc_break_lvl,int) and namelist.options.huc_break_lvl in (2,4,6,8,10,12): # override if break level specified
+                    lvl    = namelist.options.huc_break_lvl
+                    colnam = f'huc{lvl}'
+                    domain = pygeohydro.watershed.huc_wb_full(lvl)['huc'+str(lvl)]
+                    domain = domain[domain['huc'+str(lvl)].str.startswith(namelist.options.domain_hucid)]
+                else:
+                    lvl    = int(len(namelist.options.domain_hucid))
+                    colnam = f'huc{lvl}'
+                    domain = pygeohydro.WBD(f'huc{lvl}').byids(field=f'huc{lvl}',
+                                                               fids=[namelist.options.domain_hucid])
+                domain = domain.drop(columns=[col for col in domain.columns if col not in [colnam,'geometry']]) 
+                domain = domain.rename(columns = {colnam : 'domain_id'})
+                domain.to_file(namelist.fnames.domain, driver='GPKG')
+                return
+            except Exception as e_id:
+                sys.exit(f'ERROR _set_domain_boundary could not set domain from domain_hucid error {e_id}')
+        elif len(namelist.options.domain_bbox) == 4:
+            if namelist.options.verbose: print(f'  setting domain from domain_bbox {namelist.options.domain_bbox}')
+            try:
+                geom = shapely.geometry.box(namelist.options.domain_bbox[0],
+                                            namelist.options.domain_bbox[1],
+                                            namelist.options.domain_bbox[2],
+                                            namelist.options.domain_bbox[3])
+                huc_lvl = namelist.options.huc_break_lvl if namelist.options.huc_break_lvl in (2,4,6,8,10,12) else 12
+                colnam = f'huc{huc_lvl}'
+                hucs   = pygeohydro.WBD(colnam)
+                domain = hucs.bygeom(geom)
+                domain = domain.drop(columns=[col for col in domain.columns if col not in [colnam,'geometry']]) 
+                domain = domain.rename(columns={colnam : 'domain_id'})
+                domain.to_file(namelist.fnames.domain, driver='GPKG')
+                return
+            except Exception as e_bbox:
+                sys.exit(f'ERROR _set_domain_boundary could not set domain from domain_bbox error:{e_bbox}')
+        elif len(namelist.options.domain_latlon) == 2:
+            if namelist.options.verbose: print(f'  setting domain from domain_latlon {namelist.options.domain_latlon}')
+            try:
+                geom = shapely.geometry.Point(namelist.options.domain_latlon[0],
+                                            namelist.options.domain_latlon[1]).buffer(0.01)
+                huc_lvl = namelist.options.huc_break_lvl if namelist.options.huc_break_lvl in (2,4,6,8,10,12) else 12
+                colnam = f'huc{huc_lvl}'
+                hucs   = pygeohydro.WBD(colnam)
+                domain = hucs.bygeom(geom)
+                domain = domain.drop(columns=[col for col in domain.columns if col not in [colnam,'geometry']]) 
+                domain = domain.rename(columns={colnam : 'domain_id'})
+                domain.to_file(namelist.fnames.domain, driver='GPKG')
+                return
+            except Exception as e_latlon:
+                sys.exit(f'ERROR _set_domain_boundary could not set domain from domain_latlon error:{e_latlon}')
 
 def _set_paths(namelist:twtnamelist.Namelist):
     if namelist.options.verbose: print('calling _set_paths')
