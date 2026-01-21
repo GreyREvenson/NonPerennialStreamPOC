a88421a7e00b168b0d986926b02e716643735c3f
GreyREvenson <evenson.grey@epa.gov>
2026-01-21 14:29:06 -0500

    more domain def fixes

--- src/twtdomain.py
+++ src/twtdomain.py
@@ -16,13 +16,13 @@ def _set_domain(namelist:twtnamelist.Namelist):
                 if isinstance(namelist.options.huc_break_lvl,int) and namelist.options.huc_break_lvl in (2,4,6,8,10,12): # override if break level specified
                     lvl    = namelist.options.huc_break_lvl
                     colnam = f'huc{lvl}'
-                    domain = pygeohydro.watershed.huc_wb_full(lvl)['huc'+str(lvl)]
-                    domain = domain[domain['huc'+str(lvl)].str.startswith(namelist.options.domain_hucid)]
+                    domain = pygeohydro.watershed.huc_wb_full(lvl)
+                    domain = domain[domain[colnam].str.startswith(namelist.options.domain_hucid)]
                 else:
                     lvl    = int(len(namelist.options.domain_hucid))
                     colnam = f'huc{lvl}'
-                    domain = pygeohydro.WBD(f'huc{lvl}').byids(field=f'huc{lvl}',
-                                                               fids=[namelist.options.domain_hucid])
+                    domain = pygeohydro.WBD(colnam).byids(field=colnam,
+                                                          fids=[namelist.options.domain_hucid])
                 domain = domain.drop(columns=[col for col in domain.columns if col not in [colnam,'geometry']]) 
                 domain = domain.rename(columns = {colnam : 'domain_id'})
                 domain.to_file(namelist.fnames.domain, driver='GPKG')
@@ -36,7 +36,7 @@ def _set_domain(namelist:twtnamelist.Namelist):
                                             namelist.options.domain_bbox[1],
                                             namelist.options.domain_bbox[2],
                                             namelist.options.domain_bbox[3])
-                huc_lvl = namelist.options.huc_break_lvl if namelist.options.huc_break_lvl in (2,4,6,8,10,12) else 12
+                huc_lvl = namelist.options.huc_break_lvl if namelist.options.huc_break_lvl in (2,4,6,8,10,12,16) else 12
                 colnam = f'huc{huc_lvl}'
                 hucs   = pygeohydro.WBD(colnam)
                 domain = hucs.bygeom(geom)
@@ -68,13 +68,11 @@ def _set_paths(namelist:twtnamelist.Namelist):
     domain['input'] = domain['domain_id'].apply(lambda domain_id: os.path.join(namelist.dirnames.input,
                                                                                str(domain_id)))
     dirs = domain['input'].tolist()
-    if len(dirs) == 1: dirs = dirs[0]
-    os.makedirs(dirs, exist_ok=True)
+    for dir in dirs: os.makedirs(dir, exist_ok=True)
     domain['output'] = domain['domain_id'].apply(lambda domain_id: os.path.join(str(namelist.dirnames.output),
                                                                                 str(domain_id)))
     dirs = domain['output'].tolist()
-    if len(dirs) == 1: dirs = dirs[0]
-    os.makedirs(dirs, exist_ok=True)
+    for dir in dirs: os.makedirs(dir, exist_ok=True)
     if os.path.isfile(namelist.fnames.dem_user):
         domain = domain.to_crs(rasterio.open(namelist.fnames.dem_user,'r').crs.to_string())
     else:
